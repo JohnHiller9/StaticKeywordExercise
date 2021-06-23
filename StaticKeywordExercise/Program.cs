6e94deceb8cbac921fb8caac82f4b3893a97d1f6
John Hiller <jdhiller@bellsouth.net>
2021-06-23 11:30:43 -0500

    initial commit

---
+++ StaticKeywordExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticKeywordExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            Console.WriteLine($"Celsius {celsius}");
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(15);
+            Console.WriteLine($"Fahrenheit {fahrenheit}");
+        }
+    }
+}
